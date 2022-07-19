cfda993a419218b1daacb69782304f159e141e83
HosoDai <78740209+HosoDai@users.noreply.github.com>
2022-07-19 10:43:19 +0900

    fix development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,12 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # メール内のリンクをRailsの起動ポートと合わせる
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  # Letter Openerを使うように設定
+  config.action_mailer.delivery_method = :letter_opener
+
+  # Letter Openerで確認するため、実際のメール配信を行う
+  config.action_mailer.perform_deliveries = true
 end
