9e53f95fa9c6195366af5ee3516e4cccab6e8f3e
HosoDai <78740209+HosoDai@users.noreply.github.com>
2022-07-28 17:13:41 +0900

    association book user create books/index books/new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @new_book = Book.new
+  end
+
+  def create
+    new_book = Book.new(book_params)
+    new_book.user_id = current_user.id
+    new_book.save
+    redirect_to books_path
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  # ストロングパラメータ
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
   end
 end
