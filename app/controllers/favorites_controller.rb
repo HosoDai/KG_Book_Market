ab7333253f7b6ec6d20e5507e69707a6e5f3393d
HosoDai <78740209+HosoDai@users.noreply.github.com>
2022-08-17 16:22:03 +0900

    add favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+  def create
+    # favoriteのbook_idをローカル変数bookに代入
+    book = Book.find(params[:book_id])
+    # favoriteのbook_idを本のidとして生成
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
